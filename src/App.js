e30657acfc8e4661e77c4004f9bba61c50a0a1c7
poojaramnaney <poojaramnaney@gmail.com>
2023-01-22 09:17:53 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -9,15 +9,16 @@ import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
 function App() {
-    return ( <div className = "App" >
-        <NavBar />
-        <Banner />
-        <Skills />
-        <Projects />
-        <Contact />
-        <Footer />
-        </div>
-    );
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -38,19 +38,19 @@ export const Skills = () => {
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="" />
+                                <img src={meter1} alt="Image" />
                                 <h5>Web Development</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="" />
+                                <img src={meter2} alt="Image" />
                                 <h5>Brand Identity</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="" />
+                                <img src={meter3} alt="Image" />
                                 <h5>Logo Design</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="" />
+                                <img src={meter1} alt="Image" />
                                 <h5>Web Development</h5>
                             </div>
                         </Carousel>
@@ -58,7 +58,7 @@ export const Skills = () => {
                 </div>
             </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="" />
+        <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
   )
 }
