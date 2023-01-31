21e3bb319a57c054f623fbcc7db734ecd09287e5
poojaramnaney <poojaramnaney@gmail.com>
2023-01-31 15:01:51 +0530

    row added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -102,7 +102,7 @@ export const Skills = () => {
                             
 
                         </Carousel> 
-                         <div responsive={responsive} infinite={true} className="">
+                         <div class="row" responsive={responsive} infinite={true} className="">
                         <h3><br></br>Tools<br></br></h3>
                         
                         
