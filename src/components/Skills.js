d72a88349534e3f3f706ed4c5de92a5f6e0826c6
poojaramnaney <poojaramnaney@gmail.com>
2023-01-31 18:32:30 +0530

    once

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -67,7 +67,7 @@ export const Skills = () => {
                         
                         
                         <div class="box" responsive={responsive} infinite={true} className="owl-theme skill-slider">
-                            <h3 class="tagline" align="center"><br></br><br></br><br></br><br></br><br></br>UT Framework<br></br></h3>
+                            <h3 class="tagline" align="center"><br></br><br></br><br></br><br></br>UT Framework<br></br></h3>
                             <div class="column" >
                                 <img src="./images/pandas.png" alt="" />
                                 <h6>Pandas</h6>
@@ -87,7 +87,7 @@ export const Skills = () => {
                         </div>
                         
                         <div class="box" responsive={responsive} infinite={true} className="owl-theme skill-slider">
-                            <h3 class="tagline" align="center"><br></br><br></br><br></br><br></br><br></br>Database<br></br></h3>
+                            <h3 class="tagline" align="center"><br></br><br></br><br></br><br></br>Database<br></br></h3>
                             <div class="column" >
                                 <img src="./images/mysql.png" alt="" />
                                 <h6>MySQL</h6>
@@ -167,7 +167,7 @@ export const Skills = () => {
                       
                         
                         <div class="box" responsive={responsive} infinite={true} className="owl-theme skill-slider">
-                            <h3 class="tagline" align="center"><br></br><br></br><br></br>Tools<br></br></h3>
+                            <h3 class="tagline" align="center"><br></br><br></br><br></br><br></br>Tools<br></br></h3>
                                 <div class="column" >
                                     <img src="./images/git.png" alt="" />
                                     <h6>Git</h6>
